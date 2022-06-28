e67f27564d287d7c97ac731ea6e3eada543dec0a
aporve <aporvep@gmail.com>
2022-06-28 14:50:09 +0530

    Updated the code

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function () {
     $("#bodyContent").load("/views/body.html");
     
@@ -6,13 +5,4 @@ $(function () {
     scriptTag.src = "/assets/js/custom.js";
     scriptTag.type = "text/javascript";
     document.getElementById('bodyContent').append(scriptTag);
-=======
-$(function () {
-    $("#bodyContent").load("/views/body.html");
-    
-    var scriptTag = document.createElement('script');
-    scriptTag.src = "/assets/js/custom.js";
-    scriptTag.type = "text/javascript";
-    document.getElementById('bodyContent').append(scriptTag);
->>>>>>> cf48714a0e2826edfa2ee859089b70ada8cad65b
 });
