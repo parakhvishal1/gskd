cf48714a0e2826edfa2ee859089b70ada8cad65b
Ashish Bhardwaj <ashish.bhardwaj727@gmail.com>
2022-06-28 13:29:09 +0530

    splash screen + termsofuse screen

---
+++ assets/js/home.js
@@ -0,0 +1,8 @@
+$(function () {
+    $("#bodyContent").load("/views/body.html");
+    
+    var scriptTag = document.createElement('script');
+    scriptTag.src = "/assets/js/custom.js";
+    scriptTag.type = "text/javascript";
+    document.getElementById('bodyContent').append(scriptTag);
+});
