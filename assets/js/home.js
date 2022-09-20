f09d660742598a87d2e24671cf3af1e84f324479
aporve <aporvep@gmail.com>
2022-09-20 18:16:45 +0530

    Update the code with gsk data

--- assets/js/home.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $("#bodyContent").load("/gsk/views/body.html");
-    
-    var scriptTag = document.createElement('script');
-    scriptTag.src = "/gsk/assets/js/custom.js";
-    scriptTag.type = "text/javascript";
-    document.getElementById('bodyContent').append(scriptTag);
-});
