dc7f74c7c2b17900800b3125c3a67e024a03ff91
aporve <aporvep@gmail.com>
2022-11-22 23:58:53 +0530

    Added EMa code

--- assets/js/childIframe.js
+++ assets/js/childIframe.js
@@ -10,38 +10,12 @@
             `<div class="mod-head-side-bar-container">
             <div class="overflow-container-sidebar">
                 <ul class="sidebar-wrapper">
-                    <div style="text-decoration: none; color: #fff; width: 100%; padding: 10px; cursor: pointer; display: flex;"> 
-                        <img class="sidebar-img-icon" src="https://cdn.yellowmessenger.com/LS9XYE9urVcj1605084428877.png"/ style="width: 44px; height: auto;" >
-                    </div>
-                    <div id="e1" onclick="dynamicHeaderTitle(event)" class="side-bar-icon-box" title="Active Passive"> 
-                        <img class="sidebar-img-icon" src="https://cdn.yellowmessenger.com/4dnzmSDugrwW1622800597860.png"/>
-                    </div>
-                    <div  id="e2"onclick="dynamicHeaderTitle(event)" class="side-bar-icon-box" title="Asset Allocation"> 
-                        <img class="sidebar-img-icon" src="https://cdn.yellowmessenger.com/peOgkPdE8n0A1622800752931.png"/>
-                        </div>
-                    <div id="e3" onclick="dynamicHeaderTitle(event)" class="side-bar-icon-box" title="Economics"> 
-                        <img class="sidebar-img-icon" src="https://cdn.yellowmessenger.com/FPloPRu3LmfA1622800812350.png"/>
-                    </div>
-                    <div id="e4" onclick="dynamicHeaderTitle(event)" class="side-bar-icon-box" title="Investor Behaviour"> 
-                        <img class="sidebar-img-icon" src="https://cdn.yellowmessenger.com/sULHcUPQH5RL1622800858305.png"/>
-                    </div>
-                    <div id="e5" onclick="dynamicHeaderTitle(event)" class="side-bar-icon-box" title="ISG"> 
-                        <img class="sidebar-img-icon" src="https://cdn.yellowmessenger.com/GnxBWRVeIm6i1622800692538.png"/>
-                    </div>
-                    <div id="e6" onclick="dynamicHeaderTitle(event)" class="side-bar-icon-box" title="Retirement Planning"> 
-                        <img class="sidebar-img-icon" src="https://cdn.yellowmessenger.com/o2BBDya1HiQT1622800956549.png"/>
-                    </div>
-                    <div id="e7" onclick="dynamicHeaderTitle(event)" class="side-bar-icon-box" title="Strategic Communications"> 
-                        <img class="sidebar-img-icon" src="https://cdn.yellowmessenger.com/GnxBWRVeIm6i1622800692538.png"/>
-                    </div>
-                    <div id="e8" onclick="dynamicHeaderTitle(event)" class="side-bar-icon-box" title="VCMM Capital Markets"> 
-                        <img class="sidebar-img-icon" src="https://cdn.yellowmessenger.com/g6Fdrr5NeUj01622800983140.png"/>
-                    </div>
+                    <iframe id='iframe' style="width:100%; height:100%; border: none;"  src="https://aporve.github.io/gsk/index.html"></iframe>
                 </ul>
             </div>
             <div class="overflow-container-header">
                 <ul class="header-wrapper">
-                    <div class="header-title" id="main-header-title" style="font-weight: 700; font-size: large;color: black;">AskISG</div>
+                    <div class="header-title" id="main-header-title" style="font-weight: 700; font-size: large;color: black;">EMA</div>
                     <div style="display:flex;align-items:center;">
                          <div id="e11" class="icons-box"> 
                              <span class="header-bar-icon-box" title="minimize">-</span>
