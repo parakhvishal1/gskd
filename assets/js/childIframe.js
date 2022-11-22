f0fe0d90153862693435ada2002dafc8b379fa3b
aporve <aporvep@gmail.com>
2022-11-23 00:40:59 +0530

    Update the code

--- assets/js/childIframe.js
+++ assets/js/childIframe.js
@@ -16,17 +16,6 @@
             <div class="overflow-container-header">
                 <ul class="header-wrapper">
                     <div class="header-title" id="main-header-title" style="font-weight: 700; font-size: large;color: black;">EMA</div>
-                    // <div style="display:flex;align-items:center;">
-                    //      <div id="e11" class="icons-box"> 
-                    //          <span class="header-bar-icon-box" title="minimize">-</span>
-                    //      </div>
-                    //      <div id="e10" style="cursor:pointer;"> 
-                    //      <span class="header-bar-icon-box" title="maxamize"><img src="https://cdn.yellowmessenger.com/m65zoVM7Si3s1624952263631.png" style="width: 1.7rem; height: 20px;" /></span>
-                    //      </div>
-                    //      <div id="e9" class="icons-box"> 
-                    //          <span class="header-bar-icon-box" title="Close">x</span>
-                    //      </div>
-                    // </div>
                 </ul>
             </div>
             </div> `
@@ -37,7 +26,17 @@
     }
 })();
 
-
+// <div style="display:flex;align-items:center;">
+// <div id="e11" class="icons-box"> 
+//     <span class="header-bar-icon-box" title="minimize">-</span>
+// </div>
+// <div id="e10" style="cursor:pointer;"> 
+// <span class="header-bar-icon-box" title="maxamize"><img src="https://cdn.yellowmessenger.com/m65zoVM7Si3s1624952263631.png" style="width: 1.7rem; height: 20px;" /></span>
+// </div>
+// <div id="e9" class="icons-box"> 
+//     <span class="header-bar-icon-box" title="Close">x</span>
+// </div>
+// </div>
 function show_image() {
     console.error('---Blur Image---');
     var img = document.createElement("img");
