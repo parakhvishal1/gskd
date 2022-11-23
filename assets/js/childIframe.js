38168e6ba94590271cae7fdf2c916e83ed84405c
aporve <aporvep@gmail.com>
2022-11-23 16:25:19 +0530

    Update the header image

--- assets/js/childIframe.js
+++ assets/js/childIframe.js
@@ -16,7 +16,7 @@
             <div class="overflow-container-header">
                 <ul class="header-wrapper">
                   <div style="width: 100%; height:100%">
-                    <img src="https://cdn.yellowmessenger.com/v4JPE80JBwUO1667995652401.jpg" alt="GSK Image" style="width: 100%; height:100%">
+                    <img src="https://cdn.yellowmessenger.com/0y3moqbykQWK1669200810434.jpg" alt="GSK Image" style="width: 100%; height:100%">
                   </div>
                 </ul>
             </div>
