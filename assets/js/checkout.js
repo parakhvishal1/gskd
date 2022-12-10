a4305b5eebbeea90533ea5c0e57f5ec9c7d905d9
Ashish Bhardwaj <ashish.bhardwaj727@gmail.com>
2022-12-10 13:36:36 +0530

    check

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -138,7 +138,7 @@ function getAccordianAccounts(data, rebates) {
     let parsedData = getParsedData();
     let newDataJoined = [];
     if(rebates) {
-        newDataJoined = parsedData["rebates"]["orders"];
+        newDataJoined = parsedData?.["rebates"]?.["orders"];
     } else {
         newDataJoined = getJoinedCheckout(parsedData);
     }
