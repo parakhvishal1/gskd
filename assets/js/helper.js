c2c0f7ff217314b8955700831628fc2a8c27d521
aporve <aporvep@gmail.com>
2022-12-15 12:26:27 +0530

    find latest changes

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -101,8 +101,8 @@ function showDatePickerWhite(date) {
     return `
         <div class="flex calendar-picker">
             <img class="picker" src="/gskd/assets/images/svg/calendar-white.svg" />
-            <input type='text' id='tbDate' placeholder=${date ? date : 'Pick Date'} readonly="readonly" />
-            <img class="arrow-down" src="/gskd/assets/images/svg/down-white.svg" />
+            <input type='text' id='tbDate' placeholder=${date ? date : 'Pick Date'} readonly="readonly"></input>
+            <!-- <img class="arrow-down" src="/gskd/assets/images/svg/down-white.svg" /> -->
         </div>
     `;
 }
