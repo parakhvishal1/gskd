a8f588f9a2a7a4ca8de6450f8c501d20ffb60078
aporve <aporvep@gmail.com>
2023-01-02 13:44:42 +0530

    Added code for name-received

--- assets/js/parentIframe.js
+++ assets/js/parentIframe.js
@@ -229,22 +229,8 @@ window.addEventListener('message', function (eventData) {
     }
 
 
-    // if (parsedData?.event_code == 'custom-event' && parsedData?.data?.code == "bot-reloaded") {
-    //     console.log("bot-reloaded");
-    //     let data = localStorage.getItem("updated-data")
-    //     console.log('refreshed local storage data in parentIframe', JSON.parse(data));
-    //     if (!data) return;
-    //     document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
-    //         event_code: 'bot-reloaded',
-    //         data: data
-    //     }), '*');
-    //     return;
-    // }
-    // name-received
-
-    if (parsedData?.event_code == "name-received") {
-        console.error('name-received')
-        console.log("bot-reloaded event");
+    if (parsedData?.event_code == 'custom-event' && parsedData?.data?.code == "bot-reloaded") {
+        console.log("bot-reloaded");
         let data = localStorage.getItem("updated-data")
         console.log('refreshed local storage data in parentIframe', JSON.parse(data));
         if (!data) return;
@@ -252,6 +238,20 @@ window.addEventListener('message', function (eventData) {
             event_code: 'bot-reloaded',
             data: data
         }), '*');
+        return;
+    }
+    // name-received
+
+    if (parsedData?.event_code == "name-received") {
+        console.error('name-received')
+        // console.log("bot-reloaded event");
+        // let data = localStorage.getItem("updated-data")
+        // console.log('refreshed local storage data in parentIframe', JSON.parse(data));
+        // if (!data) return;
+        // document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
+        //     event_code: 'bot-reloaded',
+        //     data: data
+        // }), '*');
 
 
         window.frames.ymIframe.chat.send({
