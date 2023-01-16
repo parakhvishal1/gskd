881a8c90a544affbf91f36b521614fdb78317527
aporve <aporvep@gmail.com>
2023-01-16 18:14:22 +0530

    Added code for onload

--- assets/js/parentIframe.js
+++ assets/js/parentIframe.js
@@ -113,17 +113,17 @@ window.addEventListener('message', function (eventData) {
 
     // }
 
-    // window.onload = function () {
-    //     console.log("bot-reloaded");
-    //     let data = localStorage.getItem("updated-data")
-    //     console.log('refreshed local storage data in parentIframe', JSON.parse(data));
-    //     if (!data) return;
-    //     document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
-    //         event_code: 'bot-reloaded',
-    //         data: data
-    //     }), '*');
-    //     return;
-    // }
+    window.onload = function () {
+        console.log("bot-reloaded");
+        let data = localStorage.getItem("updated-data")
+        console.log('refreshed local storage data in parentIframe', JSON.parse(data));
+        if (!data) return;
+        document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
+            event_code: 'bot-reloaded',
+            data: data
+        }), '*');
+        return;
+    }
 
     // window.location.reload = function () {
     //     console.log('Bot-reloaded1');
