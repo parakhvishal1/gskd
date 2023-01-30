d7321072052927b1c89c58bfc8e815694851b6be
aporve <aporvep@gmail.com>
2023-01-30 21:48:55 +0530

    Added code for src

--- assets/js/parentIframe.js
+++ assets/js/parentIframe.js
@@ -480,8 +480,8 @@ window.addEventListener('message', function (eventData) {
     }
 
     if (parsedData?.event_code == 'select-brand') {
-        let source = parsedData.data.data.source;
-        console.log('select-brand Mobile', source)
+        // let source = parsedData.data.data.source;
+        // console.log('select-brand Mobile', source)
         let src = parsedData.data.source
         console.log('select-brandd src ', src)
         if (source == 'Mobile') {
@@ -508,9 +508,10 @@ window.addEventListener('message', function (eventData) {
     }
 
     if (parsedData?.event_code == 'ordercart-continue') {
-        let source = parsedData.data.data.source;
-        console.log('select-brand Mobile', source)
+        // let source = parsedData.data.data.source;
+        // console.log('select-brand Mobile', source)
         let src = parsedData.data.source
+        console.log('src', src)
         if (source == 'Mobile') {
             injectDynamicCssForMobileBot();
             console.log("\n\n\n <--- ordercart-continue event in parent iframe ---> \n\n\n", parsedData);
