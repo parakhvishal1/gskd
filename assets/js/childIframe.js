bca9f1b3168ace0a5010d8f31c82e59f32861454
aporve <aporvep@gmail.com>
2023-03-13 09:01:57 +0530

    Added New Banner and Header

--- assets/js/childIframe.js
+++ assets/js/childIframe.js
@@ -18,7 +18,7 @@
             <div class="overflow-container-header">
                 <ul class="header-wrapper">
                   <div style="width: 100%; height:100%">
-                    <img src="https://cdn.yellowmessenger.com/kYHhFpnp4oLL1673432814444.jpg" alt="GSK Image" style="width: 100%; height:100%">
+                    <img src="https://cdn.yellowmessenger.com/IfFdw9Cnnlzp1678677773281.jpg" alt="GSK Image" style="width: 100%; height:100%">
                   </div>
                 </ul>
             </div>
