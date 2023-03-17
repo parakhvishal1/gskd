8f764fd2350c2049de578e32c94a3fdf7c20129d
aporve <aporvep@gmail.com>
2023-03-17 09:27:48 +0530

    fixed data issue

--- assets/js/callers.js
+++ assets/js/callers.js
@@ -265,7 +265,7 @@ function ToApp(eventName, data, orgData) {
             loadBrandSelectionUI(data);
             break;
         case "show-brand-selection":
-            GlobalVarInit();
+            GlobalVarInit(data);
             StoreDataIn(data);
             ToApp("choosebrands-screen", data);
             break;
